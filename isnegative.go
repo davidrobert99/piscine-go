a0d70b7570d535eb4e7f68a619a50b09199cb81c
davidrobert99 <davidedamata@gmail.com>
2019-07-04 16:21:01 +0100

    isnegative

---
+++ isnegative.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/01-edu/z01"
+
+
+
+func IsNegative(nb int) {
+	if nb >= 0{
+		z01.PrintRune(rune(70))
+		z01.PrintRune('\n')
+	}else {
+		z01.PrintRune(rune(84))
+		z01.PrintRune('\n')
+	}
+}
+
+
+func main(){
+
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
+
+}
