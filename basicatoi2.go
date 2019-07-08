b187bdd2da9b42c7daaac6e2fe7c353e16f2156e
davidrobert99 <davidedamata@gmail.com>
2019-07-08 11:29:42 +0100

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,26 +1,8 @@
-package main
+package piscine
 
 import (
-	"fmt"
 	"math"
 )
-
-func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "012 345"
-	s4 := "Hello World!"
-
-	n := BasicAtoi2(s)
-	n2 := BasicAtoi2(s2)
-	n3 := BasicAtoi2(s3)
-	n4 := BasicAtoi2(s4)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
-	fmt.Println(n4)
-}
 
 func BasicAtoi2(s string) int {
 	num := 0
