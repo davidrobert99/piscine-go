0d24573da1f8171b8d365633874d398e5e29927e
davidrobert99 <davidedamata@gmail.com>
2019-07-08 14:30:53 +0100

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -1,31 +1,37 @@
-package piscine
+package main
 
 import "fmt"
 
+func main() {
+	Raid1a(-4, -4)
+}
+
 func Raid1a(x, y int) {
-	limiteHorizontal := make([]rune, x)
-	middle := make([]rune, x)
-	for i := 0; i < x; i++ {
-		if i == 0 || i == (x-1) {
-			limiteHorizontal[i] = 'o'
-		} else {
-			limiteHorizontal[i] = '-'
+	if x > 0 {
+		limiteHorizontal := make([]rune, x)
+		middle := make([]rune, x)
+		for i := 0; i < x; i++ {
+			if i == 0 || i == (x-1) {
+				limiteHorizontal[i] = 'o'
+			} else {
+				limiteHorizontal[i] = '-'
+			}
 		}
-	}
 
-	for i := 0; i < x; i++ {
-		if i == 0 || i == (x-1) {
-			middle[i] = '|'
-		} else {
-			middle[i] = ' '
+		for i := 0; i < x; i++ {
+			if i == 0 || i == (x-1) {
+				middle[i] = '|'
+			} else {
+				middle[i] = ' '
+			}
 		}
-	}
 
-	for i := 0; i < y; i++ {
-		if i == 0 || i == (y-1) {
-			fmt.Println(string(limiteHorizontal))
-		} else {
-			fmt.Println(string(middle))
+		for i := 0; i < y; i++ {
+			if i == 0 || i == (y-1) {
+				fmt.Println(string(limiteHorizontal))
+			} else {
+				fmt.Println(string(middle))
+			}
 		}
 	}
 }
