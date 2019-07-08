f054d7ab8ed0303e424362838297f3d3a526bc44
davidrobert99 <davidedamata@gmail.com>
2019-07-08 15:46:58 +0100

    IteractivePower

--- IterativePower.go
+++ IterativePower.go
@@ -1,12 +1,4 @@
-package main
-
-import "fmt"
-
-func main() {
-	arg1 := 4
-	arg2 := 3
-	fmt.Println(IterativePower(arg1, arg2))
-}
+package piscine
 
 func IterativePower(nb int, power int) int {
 	if power < 0 {
