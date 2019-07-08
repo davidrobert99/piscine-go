ccccbdf975bf1c60aaf36b1b6cde8be174f46fcc
davidrobert99 <davidedamata@gmail.com>
2019-07-08 14:52:30 +0100

    raid1e

---
+++ raid1e.go
@@ -0,0 +1,48 @@
+package piscine
+
+import "fmt"
+
+func Raid1d(x, y int) {
+	if x > 0 {
+		limiteHorizontalsuperior := make([]rune, x)
+		limiteHorizontalinferior := make([]rune, x)
+		middle := make([]rune, x)
+		for i := 0; i < x; i++ {
+			if i == 0 {
+				limiteHorizontalsuperior[i] = 'A'
+			} else if i == (x - 1) {
+				limiteHorizontalsuperior[i] = 'C'
+			} else {
+				limiteHorizontalsuperior[i] = 'B'
+			}
+		}
+
+		for i := 0; i < x; i++ {
+			if i == 0 {
+				limiteHorizontalinferior[i] = 'C'
+			} else if i == (x - 1) {
+				limiteHorizontalinferior[i] = 'A'
+			} else {
+				limiteHorizontalinferior[i] = 'B'
+			}
+		}
+
+		for i := 0; i < x; i++ {
+			if i == 0 || i == (x-1) {
+				middle[i] = 'B'
+			} else {
+				middle[i] = ' '
+			}
+		}
+
+		for i := 0; i < y; i++ {
+			if i == 0 {
+				fmt.Println(string(limiteHorizontalsuperior))
+			} else if i == (y - 1) {
+				fmt.Println(string(limiteHorizontalinferior))
+			} else {
+				fmt.Println(string(middle))
+			}
+		}
+	}
+}
