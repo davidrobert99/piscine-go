0fdfc2a6c6067ac89dbd2504ea8d95567e1de250
davidrobert99 <davidedamata@gmail.com>
2019-07-09 11:35:03 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -1,8 +1,25 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	numerosDivisores := 0
-	for i := 1; i <= nb; i++ {
+	//numerosDivisores := 0
+	if nb <= 0 {
+		return false
+	} else {
+		if nb > 3 {
+			if nb%2 == 0 || nb%3 == 0 {
+				return false
+			}
+			for i := 5; i*i < nb; i = i + 6 {
+				if nb%(i-1) == 0 || nb%(i+2) == 0 {
+					return false
+				}
+			}
+			return true
+		} else {
+			return true
+		}
+	}
+	/*for i := 1; i <= nb; i++ {
 		if nb%i == 0 {
 			numerosDivisores++
 		}
@@ -11,5 +28,5 @@ func IsPrime(nb int) bool {
 		return true
 	} else {
 		return false
-	}
+	}*/
 }
