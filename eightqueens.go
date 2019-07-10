2823b807ab0b1965ecc95fac3d21e5ff9651b78f
davidrobert99 <davidedamata@gmail.com>
2019-07-10 12:30:27 +0100

    eight

--- eightqueens.go
+++ eightqueens.go
@@ -1,89 +1,14 @@
 package piscine
 
-import "fmt"
-
-/*
-type rainha struct {
-	x int
-	y int
-}
-
-func EightQueens() {
-	rainha1 := rainha{3, 5}
-	var tabuleiro [8][8]int
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			tabuleiro[i][j] = 0
-		}
-	}
-	tabuleiro = posiciona(tabuleiro, rainha1)
-	imprimeTabuleiro(tabuleiro)
-}
-
-func imprimeTabuleiro(tabuleiro [8][8]int) {
-	for i := 0; i < 8; i++ {
-		fmt.Println(tabuleiro[i])
-	}
-}
-
-func posiciona(tabuleiro [8][8]int, r rainha) [8][8]int {
-	x1 := r.x
-	y1 := r.y
-	for x1 != 0 && y1 != 0 {
-		x1--
-		y1--
-	}
-	x2 := r.x
-	y2 := r.y
-	fmt.Print(x2)
-	fmt.Println(y2)
-	for y2 != 7 && x2 != 0 {
-
-		x2--
-		y2++
-		fmt.Print(x2)
-		fmt.Println(y2)
-	}
-
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			if i == x1 && j == y1 {
-				tabuleiro[i][j] = 2
-				x1++
-				y1++
-			}
-			fmt.Print(x2)
-			fmt.Print(y2)
-			fmt.Print(i)
-			fmt.Println(j)
-			if i == x2 && j == y2 {
-				tabuleiro[i][j] = 2
-				x2++
-				y2--
-			}
-			if i == (r.x - 1) {
-				tabuleiro[i][j] = 2
-			}
-			if j == (r.y - 1) {
-				tabuleiro[i][j] = 2
-			}
-		}
-	}
-	return tabuleiro
-}
-
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}*/
+import (
+	"github.com/01-edu/z01"
+)
 
 func EightQueens() {
 	var NaoPodeTabuleiro [8]int
 	var solucao [8]int
 	solucao = [...]int{0, 0, 0, 0, 0, 0, 0, 0}
-	for i := 1; i <= 1; i++ { //<=8
+	for i := 1; i <= 8; i++ {
 		solucao[0] = i
 		NaoPodeTabuleiro[0] = i
 		verificaSolucoes(NaoPodeTabuleiro, solucao, 1)
@@ -92,15 +17,20 @@ func EightQueens() {
 
 func verificaSolucoes(vetorNaoPode [8]int, VetorNumeroAtual [8]int, numeroAtual int) {
 	if numeroAtual == 8 { //Ultima posicao do array
-		//fmt.Println(VetorNumeroAtual)
+		imprime := true
+		for i := 0; i <= 7; i++ {
+			if VetorNumeroAtual[i] == 0 {
+				imprime = false
+			}
+		}
+		if imprime {
+			final(VetorNumeroAtual)
+		}
 	} else {
-		for i := 1; i <= 3; i++ { //<=8
-			var vetorAuxiliarNaoPode = criaVetorAuxiliar(vetorNaoPode, numeroAtual)
-			fmt.Print("Aux ")
-			fmt.Print(vetorAuxiliarNaoPode)
-			fmt.Print(" atual")
-			fmt.Println(VetorNumeroAtual)
-			if verificaSePodeInserir(vetorAuxiliarNaoPode, i) {
+		var auxiliar [8]int
+		auxiliar = criaVetorAuxiliar(vetorNaoPode, numeroAtual)
+		for i := 0; i <= 8; i++ {
+			if verificaSePodeInserir(auxiliar, i) {
 				VetorNumeroAtual[numeroAtual] = i
 				vetorNaoPode[numeroAtual] = i
 				verificaSolucoes(vetorNaoPode, VetorNumeroAtual, numeroAtual+1)
@@ -108,6 +38,13 @@ func verificaSolucoes(vetorNaoPode [8]int, VetorNumeroAtual [8]int, numeroAtual 
 		}
 	}
 
+}
+
+func final(resposta [8]int) {
+	for i := 0; i < 8; i++ {
+		z01.PrintRune(rune(resposta[i] + 48))
+	}
+	z01.PrintRune('\n')
 }
 
 func verificaSePodeInserir(vetorNaoPode [8]int, numero int) bool {
@@ -119,37 +56,55 @@ func verificaSePodeInserir(vetorNaoPode [8]int, numero int) bool {
 	return true
 }
 
-/*
-func criaVetorAuxiliar(vetorNaoPode [8]int, numeroAtual int) [8]int {
-	i := numeroAtual
-	if i <= numeroAtual {
-		if !numeroEstaVetor(vetorNaoPode, (numeroAtual-i)+vetorNaoPode[i]) {
-			if (numeroAtual-i)+vetorNaoPode[i] < 9 {
-				vetorNaoPode[i] = (numeroAtual - i) + vetorNaoPode[i]
-			}
-			if -(numeroAtual-i)+vetorNaoPode[i] > 0 {
-				vetorNaoPode[i] = -(numeroAtual - i) + vetorNaoPode[i]
+func criaVetorAuxiliar(vetorNaoPode [8]int, posicao int) [8]int {
+	var novo [8]int //Diagonal Descendente
+	novo = [...]int{0, 0, 0, 0, 0, 0, 0, 0}
+	var novo2 [8]int //Diagonal Ascendente
+	novo2 = [...]int{0, 0, 0, 0, 0, 0, 0, 0}
+	var final [8]int
+	final = [...]int{0, 0, 0, 0, 0, 0, 0, 0}
+	for e := 0; e <= 7; e++ {
+		if vetorNaoPode[e] != 0 {
+			novo[e] = vetorNaoPode[e]
+			novo2[e] = vetorNaoPode[e]
+			final[e] = vetorNaoPode[e]
+		}
+	}
+	for e := 0; e <= posicao; e++ {
+		if novo[e] != 0 {
+			if ((posicao - e) + vetorNaoPode[e]) <= 8 {
+				novo[e] = (posicao - e) + vetorNaoPode[e]
+			} else {
+				novo[e] = 0
 			}
 		}
 
 	}
+	for e := 0; e <= posicao; e++ {
+		if novo2[e] != 0 {
+			if (-(posicao - e) + vetorNaoPode[e]) >= 0 {
+				novo2[e] = -(posicao - e) + vetorNaoPode[e]
+			} else {
+				novo2[e] = 0
+			}
+		}
 
-	return vetorNaoPode
-}*/
-
-func criaVetorAuxiliar(vetorNaoPode [8]int, numeroAtual int) [8]int {
-	for i := 0; i < 8; i++ {
-		if vetorNaoPode[i] != 0 && i <= numeroAtual {
-			/*
-				if !numeroEstaVetor(vetorNaoPode, vetorNaoPode[i] +) {
-
-				}*/
-			vetorNaoPode[numeroAtual] = vetorNaoPode[i] - numeroAtual
-			fmt.Println(vetorNaoPode[i] - numeroAtual)
-			fmt.Println(vetorNaoPode[i] + numeroAtual)
+	}
+	for e := 0; e <= 7; e++ {
+		if final[e] == 0 {
+			for i := 0; i <= 7; i++ {
+				if !numeroEstaVetor(final, novo[i]) && e < 8 {
+					final[e] = novo[i]
+					e++
+				}
+				if !numeroEstaVetor(final, novo2[i]) && e < 8 {
+					final[e] = novo2[i]
+					e++
+				}
+			}
 		}
 	}
-	return vetorNaoPode
+	return final
 }
 
 func numeroEstaVetor(vetor [8]int, numero int) bool {
