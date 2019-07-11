9d15d677af511c38e038a4965583f5ec1def1bf7
davidrobert99 <davidedamata@gmail.com>
2019-07-11 17:14:04 +0100

    teste

--- teste/main.go
+++ teste/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	piscine ".."
-	"github.com/01-edu/z01"
 )
 
 func main() {
-	piscine.PrintNbrBase(125, "0123456789")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "01")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "0123456789ABCDEF")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "choumi")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "aa")
-	z01.PrintRune('\n')
+	str := "Hello how are you?"
+	table := piscine.SplitWhiteSpaces(str)
+	piscine.PrintWordsTables(table)
 }
