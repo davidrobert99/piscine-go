e398d8aac8fd72df05a67c81b6b61fb6031aa3f7
davidrobert99 <davidedamata@gmail.com>
2019-07-11 16:20:40 +0100

    sortparams

---
+++ sortparams/sortparams.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"sort"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	sort.Strings(arguments)
+	for i := 1; i < len(arguments); i++ {
+		z01.PrintRune(rune(arguments[i][0]))
+		z01.PrintRune('\n')
+	}
+}
