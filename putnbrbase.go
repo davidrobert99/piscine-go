9c13ba54a1789f0e1e6d738c87d3f47cf38592ab
davidrobert99 <davidedamata@gmail.com>
2019-07-11 12:09:38 +0100

    putnbrbase

--- putnbrbase.go
+++ putnbrbase.go
@@ -9,33 +9,45 @@ import (
 func PrintNbrBase(nbr int, base string) {
 	if len(base) > 1 {
 		aux := []rune(base)
-		inverteu := false
-		nbr1 := nbr
-		if nbr < 0 {
-			nbr = -nbr
-			inverteu = true
+		if verificaRepetidos(aux) {
+			if nbr < 0 {
+				nbr = -nbr
+				z01.PrintRune('-')
+			}
+			nbr1 := nbr
+			numeroDivisoes := 0
+			for nbr > 0 {
+				numeroDivisoes++
+				nbr = nbr / len(aux)
+			}
+			posicao := 0
+			vetor := make([]rune, numeroDivisoes)
+			for nbr1 > 0 {
+				vetor[posicao] = rune(nbr1 % len(aux))
+				nbr1 = nbr1 / len(aux)
+				posicao++
+			}
+			for i := len(vetor) - 1; i >= 0; i-- {
+				fmt.Print(string(aux[vetor[i]]))
+			}
+		} else {
+			z01.PrintRune('N')
+			z01.PrintRune('V')
 		}
-		numeroDivisoes := 0
-		for nbr > 0 {
-			numeroDivisoes++
-			nbr = nbr / len(aux)
-		}
-		posicao := 0
-		vetor := make([]rune, numeroDivisoes)
-		for nbr1 > 0 {
-			vetor[posicao] = rune(nbr1 % len(aux))
-			nbr1 = nbr1 % len(aux)
-			posicao++
-		}
-		if inverteu {
-			z01.PrintRune('-')
-
-		}
-		//faz print
-		fmt.Println(vetor)
 	} else {
 		z01.PrintRune('N')
-		z01.PrintRune('B')
-		z01.PrintRune('\n')
+		z01.PrintRune('V')
+
 	}
+}
+
+func verificaRepetidos(vetor []rune) bool {
+	for i := 0; i < len(vetor); i++ {
+		for j := i + 1; j < len(vetor); j++ {
+			if vetor[i] == vetor[j] {
+				return false
+			}
+		}
+	}
+	return true
 }
