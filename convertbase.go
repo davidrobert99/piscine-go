8ff03fd68e025f466218134b3a750fb353381530
davidrobert99 <davidedamata@gmail.com>
2019-07-11 17:35:09 +0100

    convertbase

---
+++ convertbase.go
@@ -0,0 +1,26 @@
+package piscine
+
+func ConvertBase(nbr, baseFrom, baseTo string) string {
+	valor := AtoiBase(nbr, baseFrom)
+	final := PrintNbrBaseSimples(valor, baseTo)
+	return final
+}
+
+func PrintNbrBaseSimples(nbr int, base string) string {
+	aux := []rune(base)
+	nbr1 := nbr
+	numeroDivisoes := 0
+	for nbr > 0 {
+		numeroDivisoes++
+		nbr = nbr / len(aux)
+	}
+	posicao := 0
+	vetor := make([]rune, numeroDivisoes)
+	for nbr1 > 0 {
+		vetor[posicao] = rune(nbr1%len(aux) + 48)
+		nbr1 = nbr1 / len(aux)
+		posicao++
+	}
+	final := StrRev(string(vetor))
+	return final
+}
