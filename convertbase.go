bd8e079f6f4871469807b1f7fbb379ff79f36e8e
davidrobert99 <davidedamata@gmail.com>
2019-07-12 16:40:57 +0100

    convertbase

--- convertbase.go
+++ convertbase.go
@@ -1,5 +1,7 @@
 package piscine
 
+import "fmt"
+
 func ConvertBase(nbr, baseFrom, baseTo string) string {
 	valor := AtoiBase(nbr, baseFrom)
 	final := PrintNbrBaseSimples(valor, baseTo)
@@ -17,10 +19,11 @@ func PrintNbrBaseSimples(nbr int, base string) string {
 	posicao := 0
 	vetor := make([]rune, numeroDivisoes)
 	for nbr1 > 0 {
-		vetor[posicao] = rune(nbr1%len(aux) + 48)
+		vetor[posicao] = rune(aux[nbr1%len(aux)])
 		nbr1 = nbr1 / len(aux)
 		posicao++
 	}
+	fmt.Println(string(vetor))
 	final := StrRev(string(vetor))
 	return final
 }
