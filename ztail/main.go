31f7f3ffca4defab84747fd3cb8ad2b830d515bb
davidrobert99 <davidedamata@gmail.com>
2019-07-15 15:47:40 +0100

    ztail

--- ztail/main.go
+++ ztail/main.go
@@ -1,21 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	arguments := os.Args
-	if len(arguments) == 1 {
-		reader := bufio.NewReader(os.Stdin)
-		char, _, err := reader.ReadRune()
+	if len(arguments) > 1 {
+		for i := 2; i < len(arguments); i++ {
+			file, err := os.Open(arguments[i])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				fi, err := file.Stat()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+					// nao obteve o ficheiro
+				} else {
+					valor, _ := strconv.Atoi(arguments[1])
+					if valor < 0 {
+						valor = valor * -1
+					}
+					corte := int(fi.Size()) - valor
+					if corte < 0 {
+						corte = 0
+					}
+					arr := make([]byte, fi.Size())
+					file.Read(arr)
+					if len(arguments) == 3 {
+						fmt.Print(string(arr[corte:]))
+					} else if valor != 0 {
+						fmt.Print("==> ")
+						fmt.Print(arguments[i])
+						fmt.Println(" <==")
+						fmt.Print(string(arr[corte:]))
+					}
 
-		if err != nil {
-			fmt.Println(err)
+				}
+				file.Close()
+			}
 		}
-
-		fmt.Println(string(char)
-	} else {
-		for i := 1; i < len(arguments); i++ {
-			dat, err := ioutil.ReadFile(arguments[i])
-			check(err)
-			fmt.Println(string(dat))
-			fmt.Println("")
-		}
+	} else { //nao foi passado argumentos
+		os.Exit(1)
 	}
 }
