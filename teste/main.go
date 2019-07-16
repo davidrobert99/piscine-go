e1f5f803f5ac74e3108c343dc28beb75bf1b84c4
davidrobert99 <davidedamata@gmail.com>
2019-07-16 16:57:16 +0100

    teste

--- teste/main.go
+++ teste/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	piscine ".."
 )
 
 func main() {
-	piscine.Sudoku()
+	tab1 := []int{0, 1, 2, 3, 4, 5}
+	tab2 := []int{0, 2, 1, 3}
+
+	result1 := piscine.IsSorted(f, tab1)
+	result2 := piscine.IsSorted(f, tab2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
+
+func f(a, b int) int {
+	return a - b
 }
