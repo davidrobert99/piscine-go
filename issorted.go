b44b313e133b530916fece788dee57f5a47a2e6c
davidrobert99 <davidedamata@gmail.com>
2019-07-17 10:20:53 +0100

    issorted

--- issorted.go
+++ issorted.go
@@ -1,7 +1,20 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, tab []int) bool {
+	return menorParaMaior(f, tab) || maiorParaMenor(f, tab)
+}
+
+func menorParaMaior(f func(a, b int) int, tab []int) bool {
 	for i := 1; i < len(tab)-1; i++ {
+		if f(tab[i-1], tab[i]) >= 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func maiorParaMenor(f func(a, b int) int, tab []int) bool {
+	for i := len(tab) - 1; i > 1; i-- {
 		if f(tab[i-1], tab[i]) >= 0 {
 			return false
 		}
