be68a3bce005c8a93c7fcc9fac3a2f233b039862
davidrobert99 <davidedamata@gmail.com>
2019-07-19 11:38:06 +0100

    listreverse

---
+++ listreverse.go
@@ -0,0 +1,17 @@
+package piscine
+
+func ListReverse(l *List) {
+	auxiliar := l
+	var vetor []interface{}
+	for auxiliar.Head != nil {
+		vetor = append(vetor, auxiliar.Head.Data)
+		auxiliar.Head = auxiliar.Head.Next
+	}
+	link := &List{}
+	link.Head = nil
+	link.Tail = nil
+	for i := len(vetor) - 1; i >= 0; i-- {
+		ListPushBack(link, vetor[i])
+	}
+	*l = *link
+}
