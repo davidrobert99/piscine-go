840a47f1059c666aeb5f7911b841043c691be75b
davidrobert99 <davidedamata@gmail.com>
2019-07-19 14:08:04 +0100

    collatz

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,15 +1,18 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	valor := 1
-	for start != 1 {
-		if start%2 == 0 {
-			start = start / 2
-			valor++
-		} else {
-			start = start*3 + 1
-			valor++
+	if start > 1 {
+		valor := 1
+		for start != 1 {
+			if start%2 == 0 {
+				start = start / 2
+				valor++
+			} else {
+				start = start*3 + 1
+				valor++
+			}
 		}
+		return valor
 	}
-	return valor
+	return start
 }
