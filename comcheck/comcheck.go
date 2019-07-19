1a64080b84c7770907a8916e1d3258ed13c015d7
davidrobert99 <davidedamata@gmail.com>
2019-07-19 14:12:03 +0100

    comcheck

---
+++ comcheck/comcheck.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func comcheck() {
+	arguments := os.Args
+	boolean := false
+	for i := 1; i < len(arguments); i++ {
+		if arguments[i] == "01" || arguments[i] == "galaxy" || arguments[i] == "galaxy 01" {
+			boolean = true
+		}
+	}
+	if boolean {
+		fmt.Print("Alert!!!")
+	}
+	fmt.Print("\n")
+}
+
+func main(){
+	comcheck()
+}
