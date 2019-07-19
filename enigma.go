2692b8b4c53c46981c975e8003ef8de1cb912d8c
davidrobert99 <davidedamata@gmail.com>
2019-07-19 11:33:37 +0100

    enigma

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	auxiliarA := ***a
+	auxiliarB := *b
+	auxiliarC := *******c
+	auxiliarD := ****d
+	***a = auxiliarB
+	*******c = auxiliarA
+	*b = auxiliarD
+	****d = auxiliarC
+}
