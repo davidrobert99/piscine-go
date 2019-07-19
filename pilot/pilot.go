20b20787735ee2c760aabca2bdc76d82c16d754b
davidrobert99 <davidedamata@gmail.com>
2019-07-19 14:17:13 +0100

    pilot

--- pilot/pilot.go
+++ pilot/pilot.go
@@ -10,11 +10,11 @@ type Pilot struct {
 const AIRCRAFT1 = ""
 
 func  main(){
-	var donnie student.Pilot
+	var donnie Pilot
 	donnie.Name = "Donnie"
 	donnie.Life = 100.0
 	donnie.Age = 24
-	donnie.Aircraft = student.AIRCRAFT1
+	donnie.Aircraft = AIRCRAFT1
 
 	fmt.Println(donnie)
 }
