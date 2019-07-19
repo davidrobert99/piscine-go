b2b9bd6f39e013a5f47eb1cb150c587f78dc6768
davidrobert99 <davidedamata@gmail.com>
2019-07-19 11:35:54 +0100

    listforeachif

---
+++ listforeachif.go
@@ -0,0 +1,43 @@
+package piscine
+
+func IsPositive_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNegative_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNotNumeric_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	case string, rune:
+		return true
+	}
+	return true
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	if l.Head != nil {
+		copia := l.Head
+		for copia != nil {
+			if cond(copia) {
+				f(copia)
+			}
+			copia = copia.Next
+		}
+	}
+}
