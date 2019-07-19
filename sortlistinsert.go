de81e0f6affad162938660485a386403c49f99fb
davidrobert99 <davidedamata@gmail.com>
2019-07-19 11:39:52 +0100

    sortlistinsert

---
+++ sortlistinsert.go
@@ -0,0 +1,20 @@
+package piscine
+
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+	ListPushBackNode(l, data_ref)
+	l = ListSort(l)
+	return l
+}
+
+func ListPushBackNode(node *NodeI, data int) {
+	n := &NodeI{Data: data, Next: nil}
+	if node == nil {
+		node = nil
+	} else {
+		copia := node
+		for copia.Next != nil {
+			copia = copia.Next
+		}
+		copia.Next = n
+	}
+}
