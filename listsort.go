3d76f4bcf9477c0f4f1369458cc7c1bd7762bf6b
davidrobert99 <davidedamata@gmail.com>
2019-07-19 11:38:46 +0100

    listsort

---
+++ listsort.go
@@ -0,0 +1,35 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	var numbers []int
+	percorre := l
+	for percorre != nil {
+		numbers = append(numbers, percorre.Data)
+		percorre = percorre.Next
+	}
+	SortIntegerTable(numbers)
+	l = nil
+	for i := 0; i < len(numbers); i++ {
+		l = listPushBack(l, numbers[i])
+	}
+	return l
+}
+
+func listPushBack(l *NodeI, data int) *NodeI {
+	n := &NodeI{Data: data}
+
+	if l == nil {
+		return n
+	}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+	return l
+}
