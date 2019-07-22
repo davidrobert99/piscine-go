e246e6b41e9a4263ae98e4060135e1250e046cb3
davidrobert99 <davidedamata@gmail.com>
2019-07-22 15:13:23 +0100

    listmerge

--- listmerge.go
+++ listmerge.go
@@ -1,13 +1,18 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	percorre := l1.Head
-	for percorre.Next != nil {
-		percorre = percorre.Next
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+	} else {
+		percorre := l1.Head
+		for percorre.Next != nil {
+			percorre = percorre.Next
+		}
+		percorre.Next = l2.Head
+		for percorre.Next != nil {
+			percorre = percorre.Next
+		}
+		l1.Tail = percorre
 	}
-	percorre.Next = l2.Head
-	for percorre.Next != nil {
-		percorre = percorre.Next
-	}
-	l1.Tail = percorre
 }
