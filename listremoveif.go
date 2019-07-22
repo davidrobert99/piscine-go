1863ea88a2f9ac526c157570044149207580559e
davidrobert99 <davidedamata@gmail.com>
2019-07-22 14:59:27 +0100

    listremoveif

--- listremoveif.go
+++ listremoveif.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func ListRemoveIf(l *List, data_ref interface{}) {
 	if l.Head != nil {
-
 		percorre := l.Head
 		anterior := l.Head
 		for percorre != nil {
-
 			if percorre.Data == data_ref {
 				anterior.Next = percorre.Next
+				if percorre.Next == nil {
+					l.Tail = anterior
+					if anterior != nil && anterior.Data == data_ref {
+						l.Tail = nil
+						if l.Head.Data == data_ref {
+							l.Head = l.Head.Next
+						}
+					}
+				}
 				percorre = percorre.Next
 			} else {
 				anterior = percorre
@@ -18,9 +25,7 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 			}
 		}
 	}
-	if l.Head.Data == data_ref {
-		l.Head = l.Head.Next
-	}
+
 }
 
 func PrintList(l *List) {
@@ -32,3 +37,18 @@ func PrintList(l *List) {
 
 	fmt.Print(nil, "\n")
 }
+
+/*
+	for percorre != nil {
+		if percorre.Data == data_ref {
+			anterior.Next = percorre.Next
+			if percorre.Next == nil {
+				l.Tail = anterior
+			}
+			percorre = percorre.Next
+		} else {
+			anterior = percorre
+			percorre = percorre.Next
+		}
+	}
+} */
