ef9601e75ead58d4880ae2c1774cae22378c28e0
davidrobert99 <davidedamata@gmail.com>
2019-07-22 14:42:30 +0100

    listremoveif

--- listremoveif.go
+++ listremoveif.go
@@ -1,13 +1,14 @@
 package piscine
 
+import "fmt"
+
 func ListRemoveIf(l *List, data_ref interface{}) {
 	if l.Head != nil {
-		if l.Head.Data == data_ref {
-			l.Head = l.Head.Next
-		}
+
 		percorre := l.Head
 		anterior := l.Head
 		for percorre != nil {
+
 			if percorre.Data == data_ref {
 				anterior.Next = percorre.Next
 				percorre = percorre.Next
@@ -17,4 +18,17 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 			}
 		}
 	}
+	if l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+}
+
+func PrintList(l *List) {
+	it := l.Head
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+
+	fmt.Print(nil, "\n")
 }
